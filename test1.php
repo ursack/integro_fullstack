6f9859be4eb306c6e80d6d5f2aef699cc7aabdfd
ursack <franklinbenitezkerv@gmail.com>
2021-08-30 16:43:03 -0500

    Update test1.php
    actualización de la prueba

--- test1.php
+++ test1.php
@@ -1,7 +1,12 @@
 <?php
 
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+    $invertida = strrev($input);
+    $ispal = false;
+    if($input == $invertida){
+        $ispal = true;
+    }
+    return true;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
